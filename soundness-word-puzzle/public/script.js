ca76fe67f4ccc0ffebca9cbbdb1f1d4e3c51ff87
Angelmykl <125933185+Angelmykl@users.noreply.github.com>
2025-10-23 11:22:18 +0100

    Update start game button to use correct ID

--- soundness-word-puzzle/public/script.js
+++ soundness-word-puzzle/public/script.js
@@ -418,6 +418,13 @@ window.onload = () => {
     const emptyGridHtml = Array(GRID_SIZE * GRID_SIZE).fill('<div class="grid-cell"></div>').join('');
     gridElement.innerHTML = emptyGridHtml;
     openStartModal();
-    startGameBtn.onclick = () => { closeStartModal(); startGame(); };
+
+    // CORRECT: Use #start-challenge-btn
+    document.getElementById('start-challenge-btn').onclick = () => {
+        closeStartModal();
+        startGame();
+    };
+
+    // Play Again button
     playAgainBtn.onclick = resetGame;
 };
